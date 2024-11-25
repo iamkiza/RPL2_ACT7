7e68b6cd6992e537866a9669ef849525c93a639d
iamkiza <imamzaki999@gmail.com>
2024-11-25 10:14:12 +0700

    ACT7

---
+++ src/datamahasiswa/koneksi.java
@@ -0,0 +1,25 @@
+package datamahasiswa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+    private static Connection conn;
+    public static Connection getConnect() {
+        String host = "JDBC:mysql://localhost/p4tk",
+                user = "root",
+                pass = "";
+        
+        try{
+            conn=(Connection)DriverManager.getConnection(host, user, pass);
+        }catch(SQLException err){
+            JOptionPane.showMessageDialog(null, err.getMessage());
+        }return conn;
+    }
+    
+    static Object getConnection(){
+        throw new UnsupportedOperationException("Not Suggested Yet");
+    }
+}
